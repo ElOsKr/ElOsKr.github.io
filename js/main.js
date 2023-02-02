f323089192dcd3a04db89a7d8c9f1683254e5bb7
Oscar <eloskr16@gmail.com>
2023-02-02 18:17:20 +0100

    form validation finish

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ document.addEventListener("readystatechange", cargarEventos, false);
 function cargarEventos() {
   document.getElementById("headerBtn").addEventListener("click", mobileMenu);
   window.addEventListener("scroll", percentageScroll);
+  document.getElementById("returnTop").addEventListener("click", returnTop);
+  document.getElementById("formBtn").addEventListener("click",formCheck);
 }
 
 function mobileMenu() {
-  var list = document.getElementById("headerList");
+  let list = document.getElementById("headerList");
   if (list.classList.contains("oculto")) {
     list.classList.remove("oculto");
   } else {
@@ -15,4 +17,61 @@ function mobileMenu() {
 
 function percentageScroll() {
   document.getElementById("percentageScroll").style.width = (window.scrollY * 100) / (document.body.offsetHeight - window.innerHeight) + "%";
+}
+
+function returnTop(){
+    setTimeout(()=>{
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
+        } , 200
+    )
+}
+
+async function formCheck(event){
+    let userName = document.getElementById("username");
+    let userEmail = document.getElementById("useremail");
+    let checkForm = document.getElementById("formcheck");
+    let regEx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    let flag=false;
+
+    if(userName.value==""){
+        userName.style.borderColor = "red";
+        flag=true;
+    }else{
+        userName.style.borderColor = "#95989A"
+    }
+
+    if(!regEx.test(userEmail.value)){
+        userEmail.style.borderColor = "red";
+        flag=true;
+    }else{
+        userEmail.style.borderColor = "#95989A"
+    }
+
+    if(checkForm.checked==false){
+        checkForm.style.outline = "1px solid red";
+        flag=true;
+    }else{
+        checkForm.style.outline = "none";
+    }
+
+    if(flag==false){
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            body: JSON.stringify({
+                name: userName.value,
+                email: userEmail.value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if(response.ok){
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            event.preventDefault();
+        }  
+    }
 }
