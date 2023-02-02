2910d0553c15528d87e5abc5d6832e3c945b9fa8
Oscar <eloskr16@gmail.com>
2023-02-02 15:20:08 +0100

    porcentage bar finish

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-document.addEventListener("readystatechange",cargarEventos,false);
-function cargarEventos(){
-    document.getElementById("headerBtn").addEventListener("click",mobileMenu);
+document.addEventListener("readystatechange", cargarEventos, false);
+function cargarEventos() {
+  document.getElementById("headerBtn").addEventListener("click", mobileMenu);
+  window.addEventListener("scroll", percentageScroll);
 }
 
-function mobileMenu(){
-    var list = document.getElementById("headerList");
-    if(list.classList.contains("oculto")){
-        list.classList.remove("oculto")
-        
-    }else{
-        list.classList.add("oculto")
-    }
+function mobileMenu() {
+  var list = document.getElementById("headerList");
+  if (list.classList.contains("oculto")) {
+    list.classList.remove("oculto");
+  } else {
+    list.classList.add("oculto");
+  }
+}
+
+function percentageScroll() {
+  document.getElementById("percentageScroll").style.width = (window.scrollY * 100) / (document.body.offsetHeight - window.innerHeight) + "%";
 }
