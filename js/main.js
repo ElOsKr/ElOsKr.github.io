c28d76bf2367588ffaf50f1b921309bbe391877d
Oscar <eloskr16@gmail.com>
2023-02-03 12:19:29 +0100

    modal finish

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ document.addEventListener("readystatechange", cargarEventos, false);
 function cargarEventos() {
   document.getElementById("headerBtn").addEventListener("click", mobileMenu);
   window.addEventListener("scroll", percentageScroll);
+    console.log(sessionStorage.getItem("modal"));
+  if(sessionStorage.getItem("modal") === null){
+    window.addEventListener("scroll",percentageScrollModal);
+    document.getElementById("modalClose").addEventListener("click",closeModal);
+    document.addEventListener("keydown",(event)=>{
+        if(event.key == "Escape"){
+            closeModal();
+        }
+    })
+    document.getElementById("modalBackground").addEventListener("click",(event)=>{
+        if("modalBackground" == event.target.id){
+            closeModal();
+        }
+    });
+    document.body.addEventListener("keypress",closeModal);
+    document.getElementById("modalForm").addEventListener("submit",modalSuscribe);
+  }
+
   document.getElementById("returnTop").addEventListener("click", returnTop);
   document.getElementById("contactForm").addEventListener("submit",formCheck);
 }
@@ -75,3 +93,62 @@ async function formCheck(event){
         }  
     }
 }
+
+function percentageScrollModal (){
+    let percentageScroll=(window.scrollY * 100) / (document.body.offsetHeight - window.innerHeight)
+
+    if(percentageScroll>25){
+        openModal();
+    }
+
+}
+
+
+function openModal(){
+    document.getElementById("modalBackground").style.visibility="visible";
+    document.getElementById("modalBox").style.transitionDuration="1s";
+    document.getElementById("modalBox").style.transform="translateY(25%)";
+}
+
+function closeModal(){
+
+    document.getElementById("modalBackground").style.display="none";
+    sessionStorage.setItem("modal","close");
+    
+}
+
+async function modalSuscribe(event){
+    event.preventDefault();
+    let modalError = document.getElementById("modalError");
+    let modalEmail = document.getElementById("modalEmail");
+    let regEx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    if(!regEx.test(modalEmail.value)){
+        modalEmail.style.borderColor = "red";
+        modalError.innerHTML="Error en el correo";
+    }else{
+        modalError.innerHTML="";
+        modalEmail.style.borderColor = "#95989A";
+
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            body: JSON.stringify({
+                email: modalEmail.value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if(response.ok){
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            document.getElementById("modalBox").style.transform="translateY(-25%)";
+            document.getElementById("modalBox").style.transitionDuration="0.5s";
+            setTimeout(closeModal,500);
+        }  
+
+    }
+
+
+}
+
